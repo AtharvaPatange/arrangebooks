0d155b3c96facafd2021dd5a4cf46bce606475f3
AtharvaPatange <164712914+AtharvaPatange@users.noreply.github.com>
2024-04-13 16:32:14 +0530

    first

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+
+    class Meta:
+        app_label='books'
+
+    def __str__(self):
+        return self.title
+
+
+# Create your models here.
